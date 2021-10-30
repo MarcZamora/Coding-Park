ab926e4b552be25707ffc16f4d33753a8425452a
MEZ <marczamora143@gmail.com>
2021-10-31 06:34:00 +0800

    movement w/ anim
    problem with OnCollisionEnter2D

--- Assets/Scripts/PlayerM.cs
+++ Assets/Scripts/PlayerM.cs
@@ -1,22 +1,29 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class PlayerM : MonoBehaviour
 {
  public float moveSpeed = 125.5f;
  public float jumpHeight = 10.5f;
  private Rigidbody2D body;
+ private Animator anim;
+ private bool landed;
  private void Awake()
  {
+     //grabing referense
      body = GetComponent<Rigidbody2D>();
+     anim = GetComponent<Animator>();
  } 
  private void Update()
  {
      float horizontalInput = Input.GetAxis("Horizontal");
      body.velocity = new Vector2(horizontalInput * Time.deltaTime * moveSpeed, body.velocity.y);
+
      //jumpin
-     if(Input.GetKey(KeyCode.Space))
+     if(Input.GetKey(KeyCode.Space) && landed)
      {
-         body.velocity = new Vector2(body.velocity.x, jumpHeight);
+        Jump(); 
      }
      //flip player horizontally
      if(horizontalInput > 0.01f)
@@ -27,6 +34,24 @@ public class PlayerM : MonoBehaviour
      {
          transform.localScale = new Vector3(-1,1,1);
      }
-     
+
+     //animation boolean
+    anim.SetBool("run",horizontalInput != 0);
+    anim.SetBool("landed",landed);
+       
  }
+ private void Jump()
+ {
+    body.velocity = new Vector2(body.velocity.x, jumpHeight);
+    anim.SetTrigger("jump"); 
+    landed = false;
+ }
+ private void OnCollissionEnter2D(Collission2D col)
+ {
+     if (col.gameObject.tag == "Ground")
+     {
+        landed = true;
+     }
 }
+}
+
