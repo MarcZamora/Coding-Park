58aa4d79f077251fcc20fcf652f40b836788fe49
MEZ <marczamora143@gmail.com>
2021-11-09 04:40:22 +0800

    scenemanager

---
+++ Assets/Scripts/HTML5lvlSel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HTML5lvlSel : MonoBehaviour
+{
+   public void HTML5START()
+   {
+       SceneManager.LoadScene("lvl1");
+   }
+}
