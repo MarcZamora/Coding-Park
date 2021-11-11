56c8e0610351663860422a25e4650f6da648b09a
MEZ <marczamora143@gmail.com>
2021-11-11 08:49:42 +0800

    menu and buttons overhaul

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,10 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    // Main Menu
    public void Exitbutton()
    {
        Application.Quit();
        Debug.Log("Game closed");    
    }
+
+   // public void tutorial()
+   // {
+   //     SceneManager.LoadScene("tutorial");
+   // }
+   
+   
+   //HTML LVLS
+   public void HTML5lvl1()
+   {
+       SceneManager.LoadScene("lvl1");
+   }
+   //In game MENU/UI scene transfer
+   public void ResetScene()
+   {
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+   }
+
+
   
 }
--- Assets/Scripts/PlayerM.cs
+++ Assets/Scripts/PlayerM.cs
@@ -1,16 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 
 public class PlayerM : MonoBehaviour
 {
     [SerializeField] private LayerMask groundLayer;
-    [SerializeField] private float moveSpeed;
-    public float jumpHeight;
-    private Rigidbody2D body;
+    [SerializeField] private float moveSpeed, jumpHeight;
+    private Rigidbody2D rb2d;
     private Animator anim;
-    private BoxCollider2D boxcol;
+    private PolygonCollider2D polycol;
+    private bool moveLeft, moveRight;
 
     // public GameObject[] player;
     
@@ -18,29 +17,55 @@ public class PlayerM : MonoBehaviour
     private void Awake()
     {
         //grabing referense
-        body = GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-        boxcol = GetComponent<BoxCollider2D>();
+        polycol = GetComponent<PolygonCollider2D>();
 
     } 
+
     private void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        body.velocity = new Vector2(horizontalInput * Time.deltaTime * moveSpeed, body.velocity.y);
+        rb2d.velocity = new Vector2(horizontalInput * Time.deltaTime * moveSpeed, rb2d.velocity.y);
 
         //flip player horizontally
         if(horizontalInput > 0.01f)
         {
             transform.localScale = new Vector3(5,5,5);
+        
         }
         else if(horizontalInput < -0.01f)
         {
             transform.localScale = new Vector3(-5,5,5);
+        
         }
 
         //animation boolean
         anim.SetBool("run",horizontalInput != 0);
         anim.SetBool("landed",isLanded());
+
+        
+        
+        //destroy
+        if (transform.localPosition.y <= -10)
+        {
+            Destroy(gameObject);
+        }
+
+    //controls
+        if (moveRight)
+        {
+          rb2d.velocity = new Vector2(1f * Time.deltaTime * moveSpeed, rb2d.velocity.y);
+          transform.localScale = new Vector3(5,5,5);
+          anim.SetBool("run",1f != 0);
+        }
+         if (moveLeft)
+        {
+          rb2d.velocity = new Vector2(-1f * Time.deltaTime * moveSpeed, rb2d.velocity.y);
+          transform.localScale = new Vector3(-5,5,5);
+          anim.SetBool("run",-1f != 0);
+        }
+    //controls//
 
         //jumpin
         if(Input.GetKey(KeyCode.Space) && isLanded())
@@ -48,22 +73,46 @@ public class PlayerM : MonoBehaviour
             Jump(); 
         }
         
-        //destroy
-        if (transform.localPosition.y <= -10)
-        {
-            Destroy(gameObject);
-        }
     }
-    private void Jump()
+
+    public void Jump()
     {
-        body.velocity = new Vector2(body.velocity.x, jumpHeight);
+        if (rb2d.velocity.y == 0)
+        {
+        rb2d.velocity = new Vector2(rb2d.velocity.x, jumpHeight);
         anim.SetTrigger("jump"); 
+        }
     }
     
     private bool isLanded()
     {
-        RaycastHit2D rH2D = Physics2D.BoxCast(boxcol.bounds.center, boxcol.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        RaycastHit2D rH2D = Physics2D.BoxCast(polycol.bounds.center, polycol.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
         return rH2D.collider != null;
+    }
+
+    //movecontrol
+    public void Start()
+    {
+      moveLeft = false;
+        moveRight = false;
+    }
+
+    public void MoveLeft()
+    {
+        moveLeft = true;        
+    }
+
+    public void MoveRight()
+    {
+        moveRight = true;
+    }
+
+
+    public void StopMoving()
+    {
+        moveLeft = false;
+        moveRight = false;
+        rb2d.velocity = Vector2.zero;
     }
 
     // void Start()
